7e82d8ed85f66c95029f0fe7826f8c41caad77f2
Daniel Pokorny <ext_daniel.pokorny@kontent.ai>
2025-07-03 14:18:14 +0200

    add global usings

---
+++ Kontent.Ai.Core/GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Net;
+global using System.Net.Http;
+global using System.Net.Http.Headers;
+global using System.Reflection;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Http; 
